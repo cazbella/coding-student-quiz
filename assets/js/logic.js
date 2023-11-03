349f00237492ad0a94fdd379876de7e665689075
CarolineBellaLane <carolinelane@live.co.uk>
2023-11-03 14:36:59 +0000

    begins to code highscores page functionality

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -16,12 +16,12 @@ var submitButton = document.getElementById("submit");
 var feedback = document.getElementById("feedback");
 var startScreen = document.getElementById("start-screen");
 var questionsSection = document.getElementById("questions");
-var runningScore = 0; 
+var runningScore = 0;
 
 // Audio elements
 const correctAudio = new Audio("./assets/sfx/correct.wav");
 const incorrectAudio = new Audio("./assets/sfx/incorrect.wav");
-  
+
 //The first view of the application displays a button used to start the quiz
 //this is displayed in starter code
 
@@ -30,20 +30,20 @@ const incorrectAudio = new Audio("./assets/sfx/incorrect.wav");
 //Once the quiz begins, a countdown timer starts
 // Function to start the quiz
 function startQuiz() {// Start the countdown timer
-  console.log("Start button clicked!"); 
-  
+  console.log("Start button clicked!");
+
   startScreen.classList.add("hide");
 
   // Show the first question section
-  
+
   questionsSection.classList.remove("hide");
-  
+
   startTimer();
   showQuestion();
 }
 
-  // Function to start the countdown timer
-  //Based on code from lesson
+// Function to start the countdown timer
+//Based on code from lesson
 function startTimer() {
   timeDisplay.textContent = timeLeft + ' seconds remaining';
   timerInterval = setInterval(function () {
@@ -60,15 +60,15 @@ function startTimer() {
       clearInterval(timerInterval);
       endQuiz();
     }
-    
+
   }, 1000); //interval every second
 }
 
 //show the first question
 function showQuestion() {
-  
+
   // Get the current question from the 'questions' array based on the 'currentQuestionIndex'.
-  
+
   var currentQuestion = questions[currentQuestionIndex];
   // Set the text of the 'questionTitle' element to the current question's text.
   questionTitle.textContent = currentQuestion.question;
@@ -80,22 +80,18 @@ function showQuestion() {
   currentQuestion.choices.forEach(function (choice) {
     // Create a new 'button' element.
     var choiceButton = document.createElement("button");
-    
     // Set the text content of the button to the current answer choice.
     choiceButton.textContent = choice;
-    
     // Add a CSS class "choice" to the button.
     choiceButton.setAttribute("class", "choice");
-    
     // Append the button to the 'choicesContainer' in dom to display it.
     choicesContainer.appendChild(choiceButton);
-
-    // Add a click event listener to the button to check the selected answer when it's clicked.
+    //click for answer
     choiceButton.addEventListener("click", function () {
       checkAnswer(choice, currentQuestion.correctAnswer);
     });
   });
-  
+
 }
 // showQuestion();
 
@@ -108,6 +104,7 @@ function checkAnswer(selectedChoice, correctAnswer) {
     runningScore += 10;
     console.log(runningScore);
   } else {
+    //If a question is answered incorrectly, additional time is subtracted from the timer
     incorrectAudio.play();
     feedback.textContent = "Incorrect!";
     timeLeft -= 10;
@@ -123,6 +120,7 @@ function checkAnswer(selectedChoice, correctAnswer) {
 }
 
 
+//The game ends when all questions have been answered or the timer reaches zero
 // Function to end the quiz
 function endQuiz() {
   clearInterval(timerInterval);
@@ -133,16 +131,20 @@ function endQuiz() {
   // Need to display score and initials
 }
 
-function submitScore () {
-console.log("end button clicked");
-console.log(initialsInput.value);
+function submitScore() {
+  console.log("end button clicked");
+  console.log(initialsInput.value);
 
 }
 
 
 
-submitButton.addEventListener("click", submitScore);
-   
+
+
+
+//After the game ends, the user can save their initials and score to a highscores view using local storage
+
+
 
 
 
@@ -152,11 +154,3 @@ startButton.addEventListener("click", startQuiz);
 
 
 
-
-//If a question is answered incorrectly, additional time is subtracted from the timer
-
-
-//The game ends when all questions have been answered or the timer reaches zero
-
-
-//After the game ends, the user can save their initials and score to a highscores view using local storage
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,15 +1,29 @@
 // Get DOM elements
 var highscoresList = document.getElementById("highscores");
 var clearButton = document.getElementById("clear");
+var scoreEntry = {
+  score: runningScore,
+  initials: initialsInput,
+};
+
+//create an empty array
+var highScores = []
+
+// Push the user score to the high scores array
+highScores.push(scoreEntry);
+
+//need to sort high scores from highest to lowest
+
+// change the high scores to strings
+highScores.toString
 
 // Add an event listener to the clear button
 clearButton.onclick = function () {
-  // Clear highscores logic here
+ 
 };
 
-// Function to clear highscores
 function clearHighscores() {
-  // Clear highscores code
+ 
 }
 
 
@@ -18,3 +32,5 @@ function clearHighscores() {
 window.onload = function () {
   // Load highscores from local storage and display them
 };
+
+startButton.addEventListener("click", startQuiz);
